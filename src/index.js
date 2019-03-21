322046fc44fa22b477462d4ceac1e2aaacb68eb6
Steven Fisher <steven.fisher22@gmail.com>
2019-03-21 14:13:26 -0500

    added fake data

--- src/index.js
+++ src/index.js
@@ -10,4 +10,24 @@ const App = () => {
     )
 }
 
+// Fake Data
+const Posts = [
+    {
+        id: 1,
+        title: 'Dogs are awesome',
+        website: 'dogawesome.com',
+        points: '1300',
+        author: 'steven',
+        time: '3 hours ago'
+    },
+    {
+        id: 2,
+        title: 'Cats are stupid',
+        website: 'catssuck.com',
+        points: '1500',
+        author: 'savannah',
+        time: '1 hour ago'
+    }
+]
+
 ReactDOM.render(<App />, document.querySelector('#root'))
